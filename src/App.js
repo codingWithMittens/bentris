8c9605dcb41db3ec390acedbac4263aa2ac5a905
Ben Lieber <ben.lieber@uplight.com>
2023-04-08 00:00:08 -0600

    working board with boundaries, rotation

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 
-const rows = 20;
+const rows = 10;
 const cols = 10;
 let speed = 2;
 
 const App = () => {
 
   const handleKeyDown = (event) => {
+    if (event.keyCode === 37) { //left arrow key
+      move(-1)
+    }
     if (event.keyCode === 38) { //up arrow key
       rotateActiveTetrino()
     }
+    if (event.keyCode === 39) { //right arrow key
+      move(1)
+    }
     if (event.keyCode === 40) { //down arrow key
-      spawn()
+      fall()
     }
   }
 
@@ -25,113 +31,130 @@ const App = () => {
     };
   }, [handleKeyDown]);
 
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     fall()
+  //   }, speed * 1000);
+
+  //   return () => clearInterval(intervalId);
+  // }, []);
+
   const tetrinos = [
-    {name: 'i', boundary: 4, shape: [[[1,0],[1,1],[1,2],[1,3]], [[0,2],[1,2],[2,2],[3,2]], [[2,0],[2,1],[2,2],[2,3]], [[0,1],[1,1],[2,1],[3,1]]], color: 'cyan'},
-    {name: 'j', boundary: 3, shape: [[[0,0],[1,0],[1,1],[1,2]], [[0,1],[0,2],[1,1],[2,1]], [[1,0],[1,1],[1,2],[2,2]], [[2,0],[2,1],[1,1],[0,1]]], color: 'blue'},
-    {name: 'l', boundary: 3, shape: [[[0,2],[1,0],[1,1],[1,2]], [[0,1],[2,2],[1,1],[2,1]], [[1,0],[1,1],[1,2],[2,0]], [[0,0],[2,1],[1,1],[0,1]]], color: 'orange'},
-    {name: 'o', boundary: 2, shape: [[[0,0],[0,1],[1,0],[1,1]]], color: 'yellow'},
-    {name: 's', boundary: 3, shape: [[[1,0],[1,1],[0,1],[0,2]], [[0,1],[1,1],[1,2],[2,2]], [[2,0],[2,1],[1,1],[1,2]], [[0,0],[1,0],[1,1],[2,1]]], color: 'green'},
-    {name: 't', boundary: 3, shape: [[[0,1],[1,0],[1,1],[1,2]], [[0,1],[1,1],[1,2],[2,1]], [[1,0],[1,1],[1,2],[2,1]], [[0,1],[1,1],[1,0],[2,1]]], color: 'purple'},
-    {name: 'z', boundary: 3, shape: [[[0,0],[1,0],[1,1],[1,2]], [[0,2],[1,2],[1,1],[2,1]], [[1,0],[1,1],[2,1],[2,2]], [[0,1],[1,1],[1,0],[2,0]]], color: 'red'},
+    {name: 'I', boundary: 4, shape: [[[1,0],[1,1],[1,2],[1,3]], [[0,2],[1,2],[2,2],[3,2]], [[2,0],[2,1],[2,2],[2,3]], [[0,1],[1,1],[2,1],[3,1]]], color: 'cyan'},
+    {name: 'J', boundary: 3, shape: [[[0,0],[1,0],[1,1],[1,2]], [[0,1],[0,2],[1,1],[2,1]], [[1,0],[1,1],[1,2],[2,2]], [[2,0],[2,1],[1,1],[0,1]]], color: 'blue'},
+    {name: 'L', boundary: 3, shape: [[[0,2],[1,0],[1,1],[1,2]], [[0,1],[2,2],[1,1],[2,1]], [[1,0],[1,1],[1,2],[2,0]], [[0,0],[2,1],[1,1],[0,1]]], color: 'orange'},
+    {name: 'O', boundary: 2, shape: [[[0,0],[0,1],[1,0],[1,1]]], color: 'yellow'},
+    {name: 'S', boundary: 3, shape: [[[1,0],[1,1],[0,1],[0,2]], [[0,1],[1,1],[1,2],[2,2]], [[2,0],[2,1],[1,1],[1,2]], [[0,0],[1,0],[1,1],[2,1]]], color: 'green'},
+    {name: 'T', boundary: 3, shape: [[[0,1],[1,0],[1,1],[1,2]], [[0,1],[1,1],[1,2],[2,1]], [[1,0],[1,1],[1,2],[2,1]], [[0,1],[1,1],[1,0],[2,1]]], color: 'purple'},
+    {name: 'Z', boundary: 3, shape: [[[0,0],[1,0],[1,1],[1,2]], [[0,2],[1,2],[1,1],[2,1]], [[1,0],[1,1],[2,1],[2,2]], [[0,1],[1,1],[1,0],[2,0]]], color: 'red'},
   ];
 
-  const [tetrinoSpace, setTetrinoSpace] = useState([]);
-  const [activeTetrino, setActiveTetrino] = useState( {
+  const initTetrino = () => ({
     rotation: 0,
     row: 0,
-    tetrino: tetrinos[4],
+    col: Math.max(cols / 2) - 2,
+    tetrino: tetrinos[Math.floor(Math.random() * tetrinos.length)],
   })
 
-  useEffect(() => {
-    const tetrinoBoundary = []
-    for(let i = 0; i < activeTetrino.tetrino.boundary; i++) {
-      const row = [];
-      for(let i = 0; i < activeTetrino.tetrino.boundary; i++) {
-        row[i] = { color: '', occupied: false };
-      }
-      tetrinoBoundary[i] = row;
+  const initInactiveTetrinos = []
+  for(let i = 0; i < rows; i++) {
+    const row = [];
+    for(let i = 0; i < cols; i++) {
+      row[i] = { color: ''};
     }
+    initInactiveTetrinos[i] = row;
+  }
 
-    // render tetrino into boundary
-    console.log('activeTetrino', activeTetrino)
-    activeTetrino.tetrino.shape[activeTetrino.rotation].forEach(blockPos => {
-      const [rowPos, colPos] = blockPos;
-      tetrinoBoundary[rowPos][colPos].color = activeTetrino.tetrino.color;
-    })
+  const [activeTetrino, setActiveTetrino] = useState({...initTetrino()});
+  const [inactiveTetrinos, setInactiveTetrinos] = useState(initInactiveTetrinos);
 
-    setTetrinoSpace(tetrinoBoundary)
-  }, [activeTetrino]);
+  const board = Array(rows).fill(Array(cols).fill())
 
-  // const board = Array(rows).fill(Array(cols).fill({ color: '', occupied: false }))
+  const updateActiveTetrinoAttr = (attr, newValue) => {
+    const proposedPos = {...activeTetrino, [attr]: newValue}
+    if (isValid(proposedPos.row, proposedPos.col, proposedPos.rotation)) {
+      setActiveTetrino({...activeTetrino, [attr]: newValue});
+      return true;
+    } else {
+      console.log('out of bounds')
+      return false;
+    }
+  }
 
   const rotateActiveTetrino = () => {
-    console.log('rotating')
-    setActiveTetrino({...activeTetrino, rotation: (activeTetrino.rotation + 1) % activeTetrino.tetrino.shape.length})
+    console.log('rotating', activeTetrino)
+    updateActiveTetrinoAttr('rotation', (activeTetrino.rotation + 1) % activeTetrino.tetrino.shape.length)
   };
 
   const spawn = () => {
-    console.log('new tetrino')
-    const nextTetrinoInt = Math.floor(Math.random() * tetrinos.length);
-    console.log(nextTetrinoInt)
-    setActiveTetrino({...activeTetrino, rotation: 0, tetrino: tetrinos[nextTetrinoInt]});
+    setActiveTetrino({...initTetrino()});
   };
-  // const apply = () => {};
 
-  // const isValid = () => {};
+  const move = (amount) => {
+    console.log('moving', activeTetrino)
+    updateActiveTetrinoAttr('col', activeTetrino.col + amount)
+  };
 
-  // const nextRow = () => {};
+  const applyToBoard = () => {
+    const newTetrinoMap = [...inactiveTetrinos]
+
+    tetrinoLocations(activeTetrino.row, activeTetrino.col, activeTetrino.rotation).forEach(blockPos => {
+      const [row, col] = blockPos;
+
+      newTetrinoMap[row][col] = {color: activeTetrino.tetrino.color};
+    })
+    console.log(newTetrinoMap);
+    setInactiveTetrinos(newTetrinoMap);
+  }
+
+  const fall = () => {
+    // console.log('activeTetrino', activeTetrino)
+    const newRow = activeTetrino.row + 1;
+    if (!updateActiveTetrinoAttr('row', newRow)) {
+      applyToBoard()
+      spawn();
+    }
+  };
+
+  const tetrinoLocations = (atRow, atCol, atRotation) => {
+    return activeTetrino.tetrino.shape[atRotation].map(rowPos => {
+      const [row, col] = rowPos
+      return([atRow + row, atCol + col])
+    })
+  }
+
+  const isValid = (row, col, rotation) => {
+    const proposedLocation = tetrinoLocations(row, col, rotation)
+    console.log(proposedLocation, rows)
+    const insideLeftBoundary = proposedLocation.every(coords => coords[1] >= 0);
+    const insideRightBoundary = proposedLocation.every(coords => coords[1] < cols);
+    const aboveFloor = proposedLocation.every(coords => coords[0] < rows);
+
+    return insideLeftBoundary && insideRightBoundary && aboveFloor;
+  };
+
 
   // const isRowComplete = () => {};
 
-  // const spawn = () => {
-  //   const tetrinoToSpawn = tetrinos[Math.floor(Math.random() * tetrinos.length)];
-  //   setActiveTetrino(tetrinoToSpawn);
-  // }
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     // setActiveTetrino(tetrino => {
-  //     //   const newTetrino = {...tetrino};
-  //     //   newTetrino.row = (newTetrino.row + 1) % rows;
-  //     //   return newTetrino;
-  //     // })
-
-  //     // if(rowPosition === 0) {
-  //     //   spawn();
-  //     // }
-
-  //     // const startingPos = [activeTetrino.row, Math.max(cols/2)];
-  //     // console.log(activeTetrino);
-  //     const newTetrinoSpace = [...initialTetrinoSpace];
-  //     console.log('newTetrinoSpace', newTetrinoSpace)
-  //     activeTetrino.tetrino.shape[activeTetrino.rotation].forEach(blockPos => {
-  //       console.log('blockpos', blockPos);
-  //       const [rowPos, colPos] = blockPos;
-  //       newTetrinoSpace[rowPos][colPos].occupied = true;
-  //       newTetrinoSpace[rowPos][colPos].color = activeTetrino.tetrino.color;
-  //     })
-  //     setTetrinoSpace(newTetrinoSpace);
 
 
+  const cellColor = (row, col) => {
+    const staticColors =  inactiveTetrinos[row][col].color;
+    // hack - fix this
+    const tetrinoPosition = tetrinoLocations(activeTetrino.row, activeTetrino.col, activeTetrino.rotation).map(coords => coords.join(',')).includes([row, col].join(','));
+    return tetrinoPosition ? activeTetrino.tetrino.color : staticColors;
+  }
 
-  //   }, speed * 1000);
-  //   return () => clearInterval(interval);
-  // }, []);
-
-  console.log('rendering', tetrinoSpace);
   return (
     <div className="App">
       {
-        tetrinoSpace.map((row, rowi) => {
+        board.map((row, rowi) => {
           return (
             <div className="row">
               {
                 row.map((col, coli) => {
-                  // console.log('row', row)
-                  // console.log('col', rowi, coli, col)
                   return (
-                    <div className={`col ${col.color}`}>
-                      {/* {rowPosition === i && 'CUR'} */}
+                    <div className={`col ${cellColor(rowi, coli)}`}>
                     </div>
                   )
                 })
