f8a9b76ddb6ed92518d63d12929ece78ab94a06b
Ben Lieber <ben.lieber@uplight.com>
2023-04-14 11:59:03 -0600

    mobile friendly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-
+import { useSwipeable } from 'react-swipeable';
 
 const rows = 15;
 const cols = 10;
@@ -9,24 +9,34 @@ let totalCompletedLines = 0;
 
 const App = () => {
 
+  const swipeConfig = {
+    delta: 10,                             // min distance(px) before a swipe starts. *See Notes*
+    preventScrollOnSwipe: true,           // prevents scroll during swipe (*See Details*)
+    trackTouch: true,                      // track touch input
+    trackMouse: false,                     // track mouse input
+    rotationAngle: 0,                      // set a rotation angle
+    swipeDuration: Infinity,               // allowable duration of a swipe (ms). *See Notes*
+    touchEventOptions: { passive: true },  // options for touch listeners (*See Details*)
+  }
+
+  const swipeHandlers = useSwipeable({
+    onSwiped: () => { setActive(true) },
+    onSwipedLeft: () => {move(-1)},
+    onSwipedRight: () => {move(1)},
+    onSwipedUp: () => {hardFall()},
+    onTap: () => {rotate()},
+    onSwipedDown: () => {fall()},
+    ...swipeConfig,
+  });
+
   const handleKeyDown = (event) => {
     setActive(true)
 
-    if (event.keyCode === 37) {
-      move(-1);
-    }
-    if (event.keyCode === 38) {
-      rotateActiveTetrominoe();
-    }
-    if (event.keyCode === 39) {
-      move(1);
-    }
-    if (event.keyCode === 40) {
-      fall();
-    }
-    if (event.keyCode === 32) {
-      hardFall();
-    }
+    if (event.keyCode === 37) { move(-1) }
+    if (event.keyCode === 38) { rotate() }
+    if (event.keyCode === 39) { move(1) }
+    if (event.keyCode === 40) { fall() }
+    if (event.keyCode === 32) { hardFall() }
   }
 
   useEffect(() => {
@@ -39,46 +49,39 @@ const App = () => {
 
 
   const tetrominoes = [
-    {name: 'I', shape: [[[1,0],[1,1],[1,2],[1,3]], [[0,2],[1,2],[2,2],[3,2]], [[2,0],[2,1],[2,2],[2,3]], [[0,1],[1,1],[2,1],[3,1]]], color: 'cyan'},
-    {name: 'J', shape: [[[0,0],[1,0],[1,1],[1,2]], [[0,1],[0,2],[1,1],[2,1]], [[1,0],[1,1],[1,2],[2,2]], [[2,0],[2,1],[1,1],[0,1]]], color: 'blue'},
-    {name: 'L', shape: [[[0,2],[1,0],[1,1],[1,2]], [[0,1],[2,2],[1,1],[2,1]], [[1,0],[1,1],[1,2],[2,0]], [[0,0],[2,1],[1,1],[0,1]]], color: 'orange'},
-    {name: 'O', shape: [[[0,0],[0,1],[1,0],[1,1]]], color: 'yellow'},
-    {name: 'S', shape: [[[1,0],[1,1],[0,1],[0,2]], [[0,1],[1,1],[1,2],[2,2]], [[2,0],[2,1],[1,1],[1,2]], [[0,0],[1,0],[1,1],[2,1]]], color: 'green'},
-    {name: 'T', shape: [[[0,1],[1,0],[1,1],[1,2]], [[0,1],[1,1],[1,2],[2,1]], [[1,0],[1,1],[1,2],[2,1]], [[0,1],[1,1],[1,0],[2,1]]], color: 'purple'},
-    {name: 'Z', shape: [[[0,0],[0,1],[1,1],[1,2]], [[0,2],[1,2],[1,1],[2,1]], [[1,0],[1,1],[2,1],[2,2]], [[0,1],[1,1],[1,0],[2,0]]], color: 'red'},
+    {name: 'I', shape: [[[1,0],[1,1],[1,2],[1,3]], [[0,2],[1,2],[2,2],[3,2]], [[2,0],[2,1],[2,2],[2,3]], [[0,1],[1,1],[2,1],[3,1]]]},
+    {name: 'J', shape: [[[0,0],[1,0],[1,1],[1,2]], [[0,1],[0,2],[1,1],[2,1]], [[1,0],[1,1],[1,2],[2,2]], [[2,0],[2,1],[1,1],[0,1]]]},
+    {name: 'L', shape: [[[0,2],[1,0],[1,1],[1,2]], [[0,1],[2,2],[1,1],[2,1]], [[1,0],[1,1],[1,2],[2,0]], [[0,0],[2,1],[1,1],[0,1]]]},
+    {name: 'O', shape: [[[0,0],[0,1],[1,0],[1,1]]]},
+    {name: 'S', shape: [[[1,0],[1,1],[0,1],[0,2]], [[0,1],[1,1],[1,2],[2,2]], [[2,0],[2,1],[1,1],[1,2]], [[0,0],[1,0],[1,1],[2,1]]]},
+    {name: 'T', shape: [[[0,1],[1,0],[1,1],[1,2]], [[0,1],[1,1],[1,2],[2,1]], [[1,0],[1,1],[1,2],[2,1]], [[0,1],[1,1],[1,0],[2,1]]]},
+    {name: 'Z', shape: [[[0,0],[0,1],[1,1],[1,2]], [[0,2],[1,2],[1,1],[2,1]], [[1,0],[1,1],[2,1],[2,2]], [[0,1],[1,1],[1,0],[2,0]]]},
   ];
 
   const newTetrominoeQueue = () => [...tetrominoes].sort( () => .5 - Math.random() );
-  const [tetrominoeQueue, setTetrominoeQueue] = useState(newTetrominoeQueue)
+  const [tetrominoeQueue, setTetrominoeQueue] = useState(newTetrominoeQueue);
 
   const initTetrominoe = () => ({
     rotation: 0,
     row: 0,
     col: Math.max(cols / 2) - 2,
-    // log: console.log('queue', tetrominoeQueue),
-    tetrominoe: tetrominoes[Math.floor(Math.random() * tetrominoes.length)],
-    // tetrominoe: tetrominoeQueue.pop(),
-  })
+    tetrominoe: tetrominoeQueue.shift(),
+  });
 
   const initPlacedTetrominoes = [];
   for(let i = 0; i < rows; i++) {
     initPlacedTetrominoes.push(Array(cols).fill(''));
-  }
+  };
 
-  const [activeTetrominoe, setActiveTetrominoe] = useState({...initTetrominoe()});
+  const [activeTetrominoe, setActiveTetrominoe] = useState(initTetrominoe);
+
   const [placedTetrominoes, setPlacedTetrominoes] = useState(initPlacedTetrominoes);
-  const [speed, setSpeed] = useState(1);
+  const [speed, setSpeed] = useState(.8);
   const [score, setScore] = useState(0);
-  const [level, setLevel] = useState(.4);
+  const [level, setLevel] = useState(1);
   const [active, setActive] = useState(false);
 
-  const board = Array(rows).fill(Array(cols).fill())
-
-  // useEffect(() => {
-
-  //   spawn()
-  // }, []);
-
+  const boardArray = (boardRows, boardCols) => Array(boardRows).fill(Array(boardCols).fill(''))
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -91,26 +94,27 @@ const App = () => {
   const updateActiveTetrominoeAttr = (attr, newValue) => {
     const proposedPos = {...activeTetrominoe, [attr]: newValue}
     if (isValid(proposedPos)) {
-      setActiveTetrominoe({...activeTetrominoe, [attr]: newValue});
+      setActiveTetrominoe(proposedPos);
       return true;
     } else {
       return false;
     }
   };
 
-  const rotateActiveTetrominoe = () => {
+  const rotate = () => {
     updateActiveTetrominoeAttr('rotation', (activeTetrominoe.rotation + 1) % activeTetrominoe.tetrominoe.shape.length)
   };
 
   const spawn = () => {
     const newTetrominoe = {...initTetrominoe()}
-    // const newTetrominoe = {...tetrominoeQueue.pop()}
-    // console.log('spawning')
     setActiveTetrominoe(newTetrominoe);
+    if(tetrominoeQueue.length === 0) {
+      setTetrominoeQueue([...newTetrominoeQueue()])
+    }
   };
 
   const move = (amount) => {
-    updateActiveTetrominoeAttr('col', activeTetrominoe.col + amount)
+    updateActiveTetrominoeAttr('col', activeTetrominoe.col + amount);
   };
 
   const applyToBoard = () => {
@@ -119,7 +123,7 @@ const App = () => {
     tetrominoeLocations(activeTetrominoe).forEach(blockPos => {
       const [row, col] = blockPos;
 
-      newTetrominoeMap[row][col] = activeTetrominoe.tetrominoe.color;
+      newTetrominoeMap[row][col] = `tetrominoe-${activeTetrominoe.tetrominoe.name}`;
     })
     setPlacedTetrominoes(newTetrominoeMap);
     updateLevelAndScore(removeCompleteRows());
@@ -143,27 +147,27 @@ const App = () => {
         setActive(false);
         alert('Game Over!');
         window.location.reload()
-      }
-      else {
+      } else {
         applyToBoard()
         spawn();
-      }
-    }
+      };
+    };
   };
 
   const hardFall = () => {
     let i = activeTetrominoe.row + 1;
 
     for(i; i < rows; i++) {
-      if (!isValid({...activeTetrominoe, row: i})) { break }
+      if (!isValid({...activeTetrominoe, row: i})) { break };
     };
 
-    fall(i-1)
+    fall(i-1);
+    const fallInterval = setInterval(() => (fall()), .5);
+    clearInterval(fallInterval);
   };
 
   const tetrominoeLocations = (attrs) => {
     const {row, col, rotation} = attrs;
-    // if(!activeTetrominoe.tetrominoe) {setActiveTetrominoe({...initTetrominoe()})}
     return activeTetrominoe.tetrominoe.shape[rotation]?.map(coords => [coords[0] + row, coords[1] + col]) || [];
   };
 
@@ -193,32 +197,53 @@ const App = () => {
   const cellColor = (row, col) => {
     const staticColors =  placedTetrominoes[row][col];
     const tetrominoePosition = tetrominoeLocations(activeTetrominoe).map(coords => coords.join(',')).includes([row, col].join(','));
-    const color = tetrominoePosition ? activeTetrominoe.tetrominoe.color : staticColors;
+    const color = tetrominoePosition ? `tetrominoe-${activeTetrominoe.tetrominoe.name}` : staticColors;
     const occupied = color ? 'occupied' : ''
-    return `${color} ${occupied}`
+    return `${color} ${occupied}`;
   }
 
+  const previewCellColor = (row, col) => {
+    const nextTetrominoe = tetrominoeQueue[0];
+    const shapeCoords = nextTetrominoe.shape[0];
+    const boardWidth = shapeCoords.map(coords => coords[1]).sort()[shapeCoords.length - 1]
+    const isOccupied = shapeCoords.map(coords => coords.join(',')).includes([row, col].join(','));
+    if (isOccupied) {
+      return `tetrominoe-${nextTetrominoe.name}`
+    }
+    return col > boardWidth ? 'no-display' : 'no-background';
+  }
+
+  const createBoard = (boardRows, boardCols, populationCb) => (
+    boardArray(boardRows, boardCols).map((row, rowi) => {
+      return (
+        <div className="row">
+          {
+            row.map((_, coli) => (
+              <div className={`col  ${populationCb(rowi, coli)}`}/>
+            ))
+          }
+        </div>
+      )
+    })
+  );
+
   return (
-    <div className="App">
+    <div className="App" {...swipeHandlers}>
       <div className="board">
-      {
-        board.map((row, rowi) => {
-          return (
-            <div className="row">
-              {
-                row.map((col, coli) => (
-                  <div className={`col  ${cellColor(rowi, coli)}`} />
-                ))
-              }
-            </div>
-          )
-        })
-      }
+        { createBoard(rows, cols, cellColor) }
       </div>
       <div className="info">
-        <p className="score"><span className="label">Score</span><span className="label">{score}</span></p>
-        <p className="level"><span className="label">Level</span><span className="label">{level}</span></p>
-        <p className="linesRemaining"><span className="label">Next level</span><span className="label">{(level * 10) - completedLinesInLevel}</span></p>
+        <div>
+          <p className="score"><span className="label">Score</span><span className="label">{score}</span></p>
+          <p className="level"><span className="label">Level</span><span className="label">{level}</span></p>
+          <p className="next-level"><span className="label">Next level</span><span className="label">{(level * 10) - completedLinesInLevel}</span></p>
+        </div>
+        <div>
+          <p className="next-up"><span className="label">Next up</span></p>
+          <div className="preview">
+            { createBoard(3, 4, previewCellColor) }
+          </div>
+        </div>
       </div>
     </div>
   );
